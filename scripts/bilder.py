365f3b56b2dd2eb525ad9e2db46bf4179972736b
Nils874 <grossmann-nils@gmx.de>
2024-02-12 19:49:40 +0100

    geaendert

--- scripts/bilder.py
+++ scripts/bilder.py
@@ -7,6 +7,6 @@ import variablen as vb
 aktuelle_flagge_bild_import = ImageTk.PhotoImage(Image.open("..\\bilder\\Ägypten.jpg"))
 aktuelle_flagge_bild = Label(fs.root, image=aktuelle_flagge_bild_import)
 
-liste_alle_laender = ["..\\bilder\\deutschland.jpg", "..\\bilder\\afghanistan.jpg"]
+liste_alle_laender = ["..\\bilder\\deutschland.jpg", "..\\bilder\\Bosnien und Herzegowina.jpg"]
 
 laender_europa = ["Albanien", "Andorra", "Weißrussland", "Belgien", "Bosnien und Herzegowina", "Bulgarien", "Dänemark", "Deutschland", "Estland", "Finnland", "Frankreich", "Griechenland", "Irland", "Island", "Italien", "Kasachstan", "Kosovo", "Kroatien", "Lettland", "Lichtenstein", "Litauen", "Luxemburg", "Malta", "Moldawien", "Monaco", "Montenegro", "Niederlande", "Nordmazedonien", "Norwegen", "Österreich", "Polen", "Portugal", "Rumänien", "Russland", "San Marino", "Schweden", "Schweiz", "Serbien", "Slowakei", "Slowenien", "Spanien", "Tschechien", "Türkei", "Ukraine", "Vatikanstadt"]
